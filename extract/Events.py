373b1edbe5e490f2b254fdf362a383bcb745ff9c
Vinod Mohanan <vinod.mohanan@cerner.com>
2018-03-06 14:33:46 -0500

    correcting requirements.txt

--- extract/Events.py
+++ extract/Events.py
@@ -1,4 +1,5 @@
 from extract.MapHelper import MapEvents
+from pyspark import RDD
 
 
 class Events:
@@ -10,7 +11,17 @@ class Events:
         self.dictionary = dictionary
 
     def map_events(self):
+        if self.rdd is None or self.dictionary is None:
+            raise Exception("Invalid RDD or Dictionary")
+        if not isinstance(self.rdd, RDD):
+            raise Exception("RDD passed is not an instance of RDD")
+        if not isinstance(self.dictionary,dict):
+            raise Exception("Dictionary passed is not an instance of dictionary")
         events_fields = MapEvents.get_event_fields(self.rdd)
+        if events_fields is None or not isinstance(events_fields,RDD):
+            raise Exception("events_fields is None or not an instance of RDD")
         header = events_fields.first()[1]
         mapped_events = MapEvents.map_events(events_fields,self.dictionary,header)
+        if mapped_events is None or not isinstance(mapped_events,RDD):
+            raise Exception("events_fields is None or not an instance of RDD")
         return mapped_events.groupByKey()
