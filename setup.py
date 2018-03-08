66994271058d668e57a602d805cb1d12411b5605
Vinod Mohanan <vinod.mohanan@cerner.com>
2018-03-08 16:37:19 -0500

    circleci

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version = "0.1dev",
     packages = ['util','extract','context'],
     install_requires = [
-        "pyspark == 2.0.0",
+        "pyspark == 2.2.1",
         "pytest == 3.4.0",
         "Mock == 2.0.0"
     ]
