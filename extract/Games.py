9f5ff84865731ac7d2dea90e6c4df254dcf29c3e
Vinod Mohanan <vinod.mohanan@cerner.com>
2018-03-09 15:11:03 -0500

    adding games test cases

--- extract/Games.py
+++ extract/Games.py
@@ -1,4 +1,5 @@
 from extract.MapHelper import MapGames
+from pyspark import RDD
 
 
 class Games:
@@ -7,6 +8,12 @@ class Games:
         self.rdd = rdd
 
     def map_games(self):
+        if self.rdd is None or not isinstance(self.rdd, RDD):
+            raise Exception("Invalid RDD")
         game_fields = MapGames.get_game_fields(self.rdd)
+        if game_fields is None or not isinstance(game_fields,RDD):
+            raise Exception("game_fields is None or not an instance of RDD")
         mapped_games = MapGames.map_games(game_fields)
+        if mapped_games is None or not isinstance(mapped_games,RDD):
+            raise Exception("mapped_games is None or not an instance of RDD")
         return mapped_games
---
+++ extract/test/test_Games.py
@@ -0,0 +1,79 @@
+import pytest
+from mock import patch
+from extract.Games import Games
+from extract.MapHelper import MapGames
+
+
+def test_map_games_none_rdd():
+    games = Games(None)
+    with pytest.raises(Exception):
+        games.map_games()
+
+
+def test_map_games_invalid_rdd():
+    games = Games({})
+    with pytest.raises(Exception):
+        games.map_games()
+
+
+def test_map_games_success(spark_context):
+    rdd = spark_context.parallelize([1,2,3])
+    rdd1 = spark_context.parallelize([('a',4),('b',5),('c',6)])
+    rdd2 = spark_context.parallelize([('a',7),('b',8),('c',9)])
+    games = Games(rdd)
+    with patch.object(MapGames, 'get_game_fields') as mock_game_fields:
+        with patch.object(MapGames, 'map_games') as mock_map_games:
+            mock_game_fields.return_value = rdd1
+            mock_map_games.return_value = rdd2
+            result = games.map_games().collect()
+            print(result)
+            assert len(result) == 3
+            for r in result:
+                if r[0] == 'a':
+                    assert r[1] == 7
+                elif r[0] == 'b':
+                    assert r[1] == 8
+                elif r[0] == 'c':
+                    assert r[1] == 9
+
+
+def test_map_games_none_eventfields(spark_context):
+    rdd = spark_context.parallelize([1,2,3])
+    games = Games(rdd)
+    with patch.object(MapGames, 'get_game_fields') as mock_game_fields:
+        with pytest.raises(Exception):
+            mock_game_fields.return_value = None
+            games.map_games().collect()
+
+
+def test_map_games_eventfields_notrdd(spark_context):
+    rdd = spark_context.parallelize([1,2,3])
+    games = Games(rdd)
+    with patch.object(MapGames, 'get_game_fields') as mock_game_fields:
+        with pytest.raises(Exception):
+            mock_game_fields.return_value = {}
+            games.map_games().collect()
+
+
+def test_map_games_none_mappedevents(spark_context):
+    rdd = spark_context.parallelize([1,2,3])
+    rdd1 = spark_context.parallelize([('a',4),('b',5),('c',6)])
+    games = Games(rdd)
+    with patch.object(MapGames, 'get_game_fields') as mock_game_fields:
+        with patch.object(MapGames, 'map_games') as mock_map_games:
+            with pytest.raises(Exception):
+                mock_game_fields.return_value = rdd1
+                mock_map_games.return_value = None
+                games.map_games().collect()
+
+
+def test_map_games_mappedevents_notrdd(spark_context):
+    rdd = spark_context.parallelize([1,2,3])
+    rdd1 = spark_context.parallelize([('a',4),('b',5),('c',6)])
+    games = Games(rdd)
+    with patch.object(MapGames, 'get_game_fields') as mock_game_fields:
+        with patch.object(MapGames, 'map_games') as mock_map_games:
+            with pytest.raises(Exception):
+                mock_game_fields.return_value = rdd1
+                mock_map_games.return_value = {}
+                games.map_games().collect()
