7e971e0277718f626bea04f27b5d6fac26386b23
Vinod Mohanan <vinod.mohanan@cerner.com>
2018-03-20 13:03:50 -0400

    Changes to run on Python 3

--- src/models/NamedTuples.py
+++ src/models/NamedTuples.py
@@ -1,4 +1,12 @@
 from collections import namedtuple
 
+
+# Extract
 Event = namedtuple("Event", "eventTeam opponent eventId location isGoal shotOutcome")
 Game = namedtuple("Game", "homeTeam visitingTeam season")
+
+
+# Transform
+
+PositionByTeamPerYear = namedtuple("PositionByTeamPerYear", "team year location isGoal")
+PositionByCountryPerYear = namedtuple("PositionByTeamPerYear", "country year location isGoal")
